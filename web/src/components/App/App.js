e0071e805d4f3b5876820709755b66997b9d170d
Joshua Wong <joshwonghc@gmail.com>
2020-10-21 20:02:42 +0800

    Install CRA with redux

---
+++ web/src/components/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Random from '../Random';
+import classes from './App.module.css';
+
+const App = () => (
+  <div className={classes.container}>
+    <Random />
+  </div>
+);
+
+export default App;
