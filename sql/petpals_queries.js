2b7e6b26a98d70fa006a51c92866f4fa7ddc1ff1
Tan Wei Yang <weiyang.1996@gmail.com>
2020-10-23 16:53:27 +0800

    Debug and shift out of comments

--- sql/petpals_queries.js
+++ sql/petpals_queries.js
@@ -162,63 +162,53 @@ sql.query = {
 
 
     // WEI YANG
-    search_caretaker: 'SELECT '
-    /*
-    This query is for searching for caretakers that are available,
-    Note: section about t.day::date is for generating dates in a range to check if no. of pets in each day
-    exceeds limit.
 
-    Assumptions: caretaker table has ratings attributes.
-    we let part-time caretakers with rating >= 4 to take care of up to 4 pets at a time (otherwise 2)
+    // search_caretaker: search for caretakers available during entire period,
+    // with less than 2/4/5 pets every day (for part-time/part-time rated 4/full time),
+    // matches bid price and services
 
-    variables: $1 start date, $2 end date, $3 owner username, $4 pet name, $5 max price
-
-
-    SELECT username, first_name
-    FROM users U
-    WHERE
-    $1 >= NOW() AND $2 >= $1
-      AND (
-      (EXISTS(SELECT 1 FROM full_timers F WHERE F.username = U.username)
-         AND NOT EXISTS(SELECT 1 FROM leave_dates L
-                        WHERE L.username = U.username
-                        AND (
-                          L.start_period BETWEEN $1 AND $2
-                          OR (L.start_period <= $1
-                              AND L.end_period >= $1)
-                          )
-                        )
-        )
-        OR
-        (EXISTS(SELECT 1 FROM part_timers P WHERE P.username = U.username)
-        AND EXISTS(SELECT 1 FROM available_dates A
-                   WHERE A.start_period <= $1
-                   AND A.end_period >= $2)
-        )
-      )
-
-      AND NOT EXISTS (SELECT 1 FROM generate_series(timestamp $1, timestamp $2, interval '1 day') AS T(day)
-                         WHERE (SELECT COUNT(*) FROM bookings B
-                                WHERE B.status = 'accepted'
-                                AND B.caretaker == U.username
-                                AND T.day BETWEEN B.start_period AND B.end_period) >=
-                                (SELECT CASE
-                                  WHEN EXISTS(SELECT 1 FROM full_timers F WHERE F.username = U.username) THEN 5
-                                  WHEN (SELECT C.rating FROM caretakers C WHERE C.username = B.username) >= 4 THEN 4
-                                  ELSE 2
-                                )
-                         )
-      AND EXISTS (SELECT 1 FROM handles H WHERE H.username = U.username
-                    AND H.animal_name = (SELECT type FROM pets WHERE pet_name = $4 AND owner = $3)
-                    AND H.price <= $5)
-
-      AND NOT EXISTS (SELECT 1 FROM requires R
-                        WHERE R.pet_name = $4 AND R.owner = $3
-                        AND NOT EXISTS (SELECT 1 FROM provides P
-                                        WHERE P.caretaker = U.usenname
-                                        AND P.service_name = R.service_name
-                                        AND P.animal_name = (SELECT type FROM pets WHERE pet_name = $4 AND owner = $3)
-                                        )
-                      )
-     */
+    // variables: $1 start date, $2 end date, $3 owner username, $4 pet name, $5 max price
+    search_caretaker: 'SELECT username, first_name FROM users U WHERE $1 <= $2 \
+    AND ((EXISTS(SELECT 1 FROM full_timers F WHERE F.username = U.username) \
+                    AND NOT EXISTS(SELECT 1 FROM leave_dates L WHERE L.username = U.username\
+                                AND (L.start_period BETWEEN $1 AND $2\
+                                    OR L.end_period BETWEEN $1 AND $2\
+                                    OR (L.start_period <= $1 AND L.end_period >= $1)\
+                                    )\
+                    )\
+                )\
+            OR (EXISTS(SELECT 1 FROM part_timers P WHERE P.username = U.username)\
+                AND EXISTS(SELECT 1 FROM available_dates A\
+                            WHERE A.start_period <= $1\
+                                AND A.end_period >= $2\
+                                AND A.username = U.username)\
+                )\
+    )\
+    AND NOT EXISTS (SELECT CURRENT_DATE + i \
+                    FROM generate_series(date $1 - CURRENT_DATE, date $2 - CURRENT_DATE) i\
+                         WHERE (SELECT COUNT(*) FROM bookings B\
+                                WHERE B.status = \'accepted\'\
+                                AND B.caretaker = U.username\
+                                AND CURRENT_DATE + i BETWEEN B.start_period AND B.end_period) >=\
+                                (SELECT CASE\
+                                  WHEN EXISTS(SELECT 1 FROM full_timers F WHERE F.username = U.username) THEN 5\
+                                  WHEN (SELECT C.average_rating FROM caretakers C WHERE C.username = U.username) >= 4 THEN 4\
+                                  ELSE 2\
+                                  END\
+                                )\
+\
+    )\
+    AND EXISTS (SELECT 1 FROM handles H WHERE H.caretaker = U.username\
+                AND H.animal_name = (SELECT type FROM pets WHERE pet_name = $4 AND owner = $3)\
+                AND H.price <= $5)\
+\
+    AND NOT EXISTS (SELECT 1 FROM requires R\
+                    WHERE R.pet_name = $4 AND R.owner = $3\
+                    AND NOT EXISTS (SELECT 1 FROM provides P\
+                                        WHERE P.caretaker = U.username\
+                                        AND P.service_name = R.service_name\
+                                        AND P.animal_name = (SELECT type FROM pets WHERE pet_name = $4 AND owner = $3)\
+                                        )\
+                      )\
+    '
 }
