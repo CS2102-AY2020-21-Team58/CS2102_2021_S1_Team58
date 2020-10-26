de550e5dc0850b1f3b95159a69d987d07ca2137f
Abhiman2211 <49817648+Abhiman2211@users.noreply.github.com>
2020-10-26 16:30:12 +0800

    Caretaker Queries (#3)
    * implement check on caretaker minimum animal price
    
    * add checks for bookings table
    
    * add salary and pet days queries
    
    * add trigger for caretaker price update
    
    * add full timer less than 60 days query
    
    * add auto accept trigger
    
    * add decline clashing bids query
    
    * fix miscellaneous queries

--- sql/petpals.sql
+++ sql/petpals.sql
@@ -50,10 +50,22 @@ CREATE TABLE services (
     service_name varchar(64) PRIMARY KEY
 );
 
+CREATE FUNCTION getMinimumPrice(varchar)
+RETURNS NUMERIC AS $$
+  DECLARE price NUMERIC;
+  BEGIN
+          SELECT  base_price INTO price
+          FROM    pet_types
+          WHERE   animal_name = $1;
+
+          RETURN price;
+  END;
+$$ LANGUAGE plpgsql;
+
 CREATE TABLE handles (
     caretaker   varchar(64) references caretakers(username),
     animal_name varchar(64) references pet_types(animal_name),
-    price       numeric     NOT NULL check(price > 0),
+    price       numeric     NOT NULL check(price >= getMinimumPrice(animal_name)),
     PRIMARY KEY(caretaker, animal_name)
 );
 
@@ -80,21 +92,81 @@ CREATE TABLE requires (
     PRIMARY KEY(owner, pet_name, service_name)
 );
 
+CREATE FUNCTION getMinimumAskingPrice(varchar, varchar, varchar)
+RETURNS NUMERIC AS $$
+  DECLARE rate NUMERIC;
+  BEGIN
+          SELECT  price INTO rate
+          FROM    handles
+          WHERE   caretaker = $1 AND animal_name = (SELECT type FROM pets WHERE pet_name = $3 AND owner = $2);
+
+          RETURN rate;
+  END;
+$$ LANGUAGE plpgsql;
+
 CREATE TABLE bookings (
     owner varchar(64),
     pet_name varchar(64),
     caretaker varchar(64) references caretakers(username),
     start_period date, 
-    end_period date,
+    end_period date check(end_period >= start_period),
     payment_method varchar(64) NOT NULL,
     delivery_method varchar(64) NOT NULL,
     status varchar(64) NOT NULL,
-    bid_rate numeric NOT NULL,
+    bid_rate numeric NOT NULL check(bid_rate >= getMinimumAskingPrice(caretaker, owner, pet_name)),
     rating numeric check(rating >= 0 AND rating <= 5),
     remarks varchar(1000),
     FOREIGN KEY(owner, pet_name) references pets(owner, pet_name),
     PRIMARY KEY(owner, pet_name, caretaker, start_period, end_period)
 );
+
+CREATE OR REPLACE FUNCTION update_caretaker_rates_on_new() RETURNS trigger AS $ret$
+	BEGIN
+		UPDATE handles
+		SET price=NEW.base_price
+		WHERE price < NEW.base_price;
+        RETURN NEW;
+	END;    
+$ret$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_caretaker_rates
+	AFTER UPDATE ON pet_types
+	FOR EACH ROW
+	EXECUTE PROCEDURE update_caretaker_rates_on_new();
+
+
+CREATE OR REPLACE FUNCTION update_fulltimer_booking() RETURNS trigger as $ret$
+    BEGIN
+        IF NEW.caretaker IN (SELECT username FROM full_timers) AND 
+            NOT EXISTS (SELECT *
+                        FROM (SELECT NEW.start_period + (interval '1' month * generate_series(0, CAST((DATE_PART('month', NEW.end_period) - DATE_PART('month', NEW.start_period)) AS INTEGER))) AS day) AS interval_months
+                        WHERE (SELECT SUM(
+                                    CASE
+                                    WHEN DATE_PART('month', interval_months.day) = DATE_PART('month', start_period) AND (DATE_PART('month', interval_months.day) < DATE_PART('month', end_period) OR DATE_PART('year', interval_months.day) < DATE_PART('year', end_period)) THEN
+                                    DATE_PART('day', (date_trunc('month', start_period) + interval '1 month') - start_period)
+                                    WHEN DATE_PART('month', interval_months.day) = DATE_PART('month', end_period) AND (DATE_PART('month', interval_months.day) > DATE_PART('month', start_period) OR DATE_PART('year', interval_months.day) > DATE_PART('year', end_period)) THEN
+                                    DATE_PART('day', end_period)
+                                    WHEN DATE_PART('month', start_period) = DATE_PART('month', interval_months.day) AND DATE_PART('month', end_period) = DATE_PART('month', interval_months.day) AND DATE_PART('year', start_period) = DATE_PART('year', end_period) THEN
+                                    end_period - start_period + 1
+                                    WHEN DATE_PART('month', start_period) < DATE_PART('month', interval_months.day) AND DATE_PART('month', end_period) > DATE_PART('month', interval_months.day) AND DATE_PART('year', start_period) <= DATE_PART('year', interval_months.day) AND DATE_PART('year', end_period) >= DATE_PART('year', interval_months.day) THEN
+                                    DATE_PART('day', date_trunc('month', interval_months.day) + interval '1 month' - interval '1 day')
+                                    END)
+                                FROM (SELECT * FROM bookings EXCEPT SELECT * FROM bookings WHERE NEW.owner=owner AND NEW.pet_name=pet_name AND NEW.caretaker=caretaker AND NEW.start_period=start_period AND NEW.end_period=end_period) AS b
+                                WHERE caretaker=NEW.caretaker AND status='ACCEPTED') > 60) AND 
+            NOT EXISTS (SELECT *
+                        FROM (SELECT NEW.start_period + (interval '1' day * generate_series(0, (CAST(DATE_PART('day', NEW.end_period) AS INTEGER) - CAST(DATE_PART('day', NEW.start_period) AS INTEGER)))) AS days) AS dates
+                        WHERE (SELECT COUNT(*) FROM bookings b WHERE b.caretaker=NEW.caretaker AND b.start_period<=dates.days AND b.end_period>=dates.days) = 5)
+        THEN UPDATE bookings SET "status" = 'ACCEPTED' WHERE NEW.owner=owner AND NEW.pet_name=pet_name AND NEW.caretaker=caretaker AND NEW.start_period=start_period AND NEW.end_period=end_period;
+        END IF;
+        RETURN NEW;
+    END;
+$ret$ LANGUAGE plpgsql;
+    
+CREATE TRIGGER direct_accept
+    AFTER INSERT ON bookings
+    FOR EACH ROW
+    EXECUTE PROCEDURE update_fulltimer_booking();
+
 
 CREATE OR REPLACE FUNCTION not_full_timer() RETURNS TRIGGER AS $trig$
 DECLARE ctx NUMERIC;
@@ -148,4 +220,30 @@ CREATE OR REPLACE FUNCTION update_avg_rating() RETURNS trigger AS $ret$
  	AFTER INSERT OR UPDATE ON bookings
  	FOR EACH ROW
  	EXECUTE PROCEDURE update_avg_rating();
+
+CREATE OR REPLACE FUNCTION decline_clashing() RETURNS trigger AS $ret$
+	BEGIN
+		IF EXISTS (SELECT 1 FROM full_timers WHERE username=NEW.caretaker)
+            THEN UPDATE bookings b1 SET "status" = 'DECLINED' WHERE "status"='PENDING' AND
+                caretaker=NEW.caretaker AND
+                EXISTS (SELECT 1
+                    FROM (SELECT b1.start_period + (interval '1' day * generate_series(0, (CAST(DATE_PART('day', b1.end_period) AS INTEGER) - CAST(DATE_PART('day', b1.start_period) AS INTEGER)))) AS days) AS dates
+                    WHERE (SELECT COUNT(*) FROM bookings b WHERE b.status='ACCEPTED' AND b.caretaker=NEW.caretaker AND b.start_period<=dates.days AND b.end_period>=dates.days) = 5);
+        ELSE
+            UPDATE bookings b2 SET "status" = 'DECLINED' WHERE "status=PENDING" AND
+                caretaker=NEW.caretaker AND
+                EXISTS (SELECT 1
+                    FROM (SELECT b2.start_period + (interval '1' day * generate_series(0, (CAST(DATE_PART('day', b2.end_period) AS INTEGER) - CAST(DATE_PART('day', b2.start_period) AS INTEGER)))) AS days) AS dates
+                    WHERE (SELECT COUNT(*) FROM bookings b WHERE b.status='ACCEPTED' AND b.caretaker=NEW.caretaker AND b.start_period<=dates.days AND b.end_period>=dates.days) = (SELECT CASE WHEN (SELECT average_rating FROM caretakers WHERE username=NEW.caretaker) >= 4.0 THEN 4 ELSE 2 END));
+                      
+        END IF;
+    RETURN NEW;
+	END;    
+$ret$ LANGUAGE plpgsql;
+
+CREATE TRIGGER decline_clashing_bids
+	AFTER UPDATE OF "status" ON bookings
+	FOR EACH ROW
+	EXECUTE PROCEDURE decline_clashing();
+
      
--- sql/petpals_queries.js
+++ sql/petpals_queries.js
@@ -1,3 +1,7 @@
+const STATUS_ACCEPTED = "ACCEPTED";
+const STATUS_PENDING = "PENDING";
+const STATUS_DECLINED = "DECLINED";
+
 const sql = {}
 
 sql.query = {
@@ -570,6 +574,51 @@ WHERE caretaker = C.username AND status = \'ACCEPTED\') <= 60 \
 
     // ABHIMAN
 
+    // $1 = Date in the month for which you need the data - give full date in YYYY-MM-DD format.
+    caretakers_with_below_60: `SELECT DISTINCT u.username, u.first_name, (CASE WHEN b.sum IS NULL THEN 0 ELSE b.sum END) AS Days \
+    FROM (users u NATURAL JOIN full_timers f) LEFT JOIN \
+    (SELECT caretaker, SUM( \
+      CASE \
+        WHEN DATE_PART(\'month\', TIMESTAMP $1) = DATE_PART(\'month\', start_period) AND (DATE_PART(\'month\', TIMESTAMP $1) < DATE_PART(\'month\', end_period) OR DATE_PART(\'year\', TIMESTAMP $1) < DATE_PART(\'year\', end_period)) THEN \
+          DATE_PART(\'day\', (date_trunc(\'month\', start_period) + interval \'1 month\') - start_period) \
+        WHEN DATE_PART(\'month\', TIMESTAMP $1) = DATE_PART(\'month\', end_period) AND (DATE_PART(\'month\', TIMESTAMP $1) > DATE_PART(\'month\', start_period) OR DATE_PART(\'year\', TIMESTAMP $1) > DATE_PART(\'year\', end_period)) THEN \
+          DATE_PART(\'day\', end_period) \
+        WHEN DATE_PART(\'month\', start_period) = DATE_PART(\'month\', TIMESTAMP $1) AND DATE_PART(\'month\', end_period) = DATE_PART(\'month\', TIMESTAMP $1) AND DATE_PART(\'year\', start_period) = DATE_PART(\'year\', end_period) THEN \
+          (end_period - start_period + 1) \
+        WHEN DATE_PART(\'month\', start_period) < DATE_PART(\'month\', TIMESTAMP $1) AND DATE_PART(\'month\', end_period) > DATE_PART(\'month\', TIMESTAMP $1) AND DATE_PART(\'year\', start_period) <= DATE_PART(\'year\', TIMESTAMP $1) AND DATE_PART(\'year\', end_period) >= DATE_PART(\'year\', TIMESTAMP $1) THEN \
+          DATE_PART(\'day\', date_trunc(\'month\', TIMESTAMP $1) + interval \'1 month\' - interval \'1 day\') \
+      END) \
+    FROM bookings \
+    WHERE status=${STATUS_ACCEPTED}\
+    GROUP BY caretaker) as b ON b.caretaker=f.username \
+    WHERE b.sum < 60 OR b.sum IS NULL`,
+
+    // $1 = Name of caretaker
+    // $2 = Date in the month for which you need the data
+    pet_days_for_month: `SELECT SUM( \
+        CASE \
+            WHEN DATE_PART(\'month\', TIMESTAMP $2) = DATE_PART(\'month\', start_period) AND (DATE_PART(\'month\', TIMESTAMP $2) < DATE_PART(\'month\', end_period) OR DATE_PART(\'year\', TIMESTAMP $2) < DATE_PART(\'year\', end_period)) THEN \
+                DATE_PART(\'day\', (date_trunc(\'month\', start_period) + interval \'1 month\') - start_period) \
+            WHEN DATE_PART(\'month\', TIMESTAMP $2) = DATE_PART(\'month\', end_period) AND (DATE_PART(\'month\', TIMESTAMP $2) > DATE_PART(\'month\', start_period) OR DATE_PART(\'year\', TIMESTAMP $2) > DATE_PART(\'year\', end_period)) THEN \
+                DATE_PART(\'day\', end_period) \
+            WHEN DATE_PART(\'month\', start_period) = DATE_PART(\'month\', TIMESTAMP $2) AND DATE_PART(\'month\', end_period) = DATE_PART(\'month\', TIMESTAMP $2) AND DATE_PART(\'year\', start_period) = DATE_PART(\'year\', end_period) THEN \
+                end_period - start_period + 1 \
+            WHEN DATE_PART(\'month\', start_period) < DATE_PART(\'month\', TIMESTAMP $2) AND DATE_PART(\'month\', end_period) > DATE_PART(\'month\', TIMESTAMP $2) AND DATE_PART(\'year\', start_period) <= DATE_PART(\'year\', TIMESTAMP $2) AND DATE_PART(\'year\', end_period) >= DATE_PART(\'year\', TIMESTAMP $2) THEN \
+                DATE_PART(\'day\', date_trunc(\'month\', TIMESTAMP $2) + interval \'1 month\' - interval \'1 day\') \
+        END) \
+        FROM bookings \
+        WHERE caretaker = $1 AND status = ${STATUS_ACCEPTED}`,
+
+    // $1 = Name of caretaker
+    caretaker_pending_bids: `SELECT caretaker, owner, pet_name, start_period, end_period, payment_method, delivery_method, bid_rate \
+                            FROM bookings \
+                            WHERE caretaker = $1 AND status = ${STATUS_PENDING}`,
+
+    // $1 = Name of caretaker
+    // $2 = Date in the month for which you need the data
+    accepted_bids_for_month: `SELECT caretaker, owner, pet_name, start_period, end_period, payment_method, delivery_method, bid_rate, rating, remarks \
+                            FROM bookings \
+                            WHERE caretaker = $1 AND (DATE_PART(\'month\',TIMESTAMP $2) = DATE_PART(\'month\', start_period) OR DATE_PART(\'month\',TIMESTAMP $2) = DATE_PART(\'month\', end_period)) AND DATE_PART(\'year\', TIMESTAMP $2) = DATE_PART(\'year\', start_period) AND status = ${STATUS_ACCEPTED}`
 
     // WEI YANG
 
