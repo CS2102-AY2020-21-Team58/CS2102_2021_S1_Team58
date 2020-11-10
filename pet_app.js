30445a70dc9c91b1027f01349e236dc5817f25cd
Tan Wei Yang <weiyang.1996@gmail.com>
2020-11-10 22:57:21 +0800

    Change backend port settings for deployment

--- pet_app.js
+++ pet_app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const pet_router = require("./routes/pet_init");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
